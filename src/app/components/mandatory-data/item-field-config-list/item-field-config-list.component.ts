dd6b2d9e5b9985466b0732fe3a4f579d21ac92b1
Oleksandr Kordunyan <kordunyanolexandr@gmail.com>
2018-10-24 18:35:57 +0300

    Merged item field config list component

--- src/app/components/mandatory-data/item-field-config-list/item-field-config-list.component.ts
+++ src/app/components/mandatory-data/item-field-config-list/item-field-config-list.component.ts
@@ -10,18 +10,7 @@ import {LanguageHttpService} from 'src/app/shared/service/http/language-http.ser
 import {Language} from 'src/app/shared/domain/language';
 import {ProgressBarService} from 'src/app/shared/service/progress-bar.service';
 import {ItemHttpService} from '../../../shared/service/http/item-http.service';
-import { Component, OnInit } from '@angular/core';
-import { ItemFieldConfigHttpService } from 'src/app/shared/service/http/item-field-config-http.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { forkJoin } from 'rxjs';
-import { first, switchMap } from 'rxjs/operators';
-import { AppProperties } from 'src/app/shared/domain/app-properties';
-import { ItemFieldConfig } from 'src/app/shared/domain/item-field-config';
-import { FieldConfigHttpService } from 'src/app/shared/service/http/field-config-http.service';
-import { LanguageHttpService } from 'src/app/shared/service/http/language-http.service';
-import { Language } from 'src/app/shared/domain/language';
-import { ProgressBarService } from 'src/app/shared/service/progress-bar.service';
-import { FieldConfig } from 'src/app/shared/domain/field-config';
+import {FieldConfig} from 'src/app/shared/domain/field-config';
 
 @Component({
   selector: 'app-item-field-config-list',
@@ -55,12 +44,12 @@ export class ItemFieldConfigListComponent implements OnInit {
         switchMap((params: ParamMap) => {
           return this.itemFieldConfigHttpService.getInstructionsByItemId(params.get(AppProperties.REQUEST_PARAM_ITEM_ID));
         })),
-        this.fieldConfigHttpService.getInstructionsFields(),
-        this.fieldConfigHttpService.getInstructionsFieldConfigsMap(),
-        this.languageHttpService.getAll()
+      this.fieldConfigHttpService.getInstructionsFields(),
+      this.fieldConfigHttpService.getInstructionsFieldConfigsMap(),
+      this.languageHttpService.getAll()
     ).subscribe((result) => {
-      //result[0] = item field configs, result[1] = instructions fields, result[2] = instructions field configs map, result[3] = languages
-      this.itemFieldConfigs = result[0];   
+      // result[0] = item field configs, result[1] = instructions fields, result[2] = instructions field configs map, result[3] = languages
+      this.itemFieldConfigs = result[0];
       this.instructionsFields = result[1];
       this.instructionsFieldCofigsMap = result[2];
       this.languages = result[3];
